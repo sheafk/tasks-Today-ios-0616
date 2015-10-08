6427cf3b233afe6843c429c29a69a82ef09a7465
markedwardmurray <markedwardmurray@gmail.com>
2015-10-08 11:45:25 -0400

    merge master into solution, retrieve solution from 6000066

---
+++ tasksToday/FISEventsViewController.h
@@ -0,0 +1,14 @@
+//
+//  FISEventsViewController.h
+//  tasksToday
+//
+//  Created by Joe Burgess on 6/19/14.
+//  Copyright (c) 2014 Joe Burgess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISEventsViewController : UIViewController
+
+@property (strong, nonatomic) NSArray *events;
+@end
